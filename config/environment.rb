5354095e0c9db109e1c82df7f211610510d6684e
ddaoellena <ddaoellena@gmail.com>
2015-11-07 11:22:01 +0100

    navbar

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#désactiver en local pour mailcatcher
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'morning-atoll-2425.herokuapp.com',
-  :enable_starttls_auto => true
+ :address        => 'smtp.sendgrid.net',
+ :port           => '587',
+ :authentication => :plain,
+ :user_name      => ENV['SENDGRID_USERNAME'],
+ :password       => ENV['SENDGRID_PASSWORD'],
+ :domain         => 'morning-atoll-2425.herokuapp.com',
+ :enable_starttls_auto => true
 }
