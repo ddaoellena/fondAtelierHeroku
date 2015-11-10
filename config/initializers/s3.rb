7fce5b04430b305532aa4d4fd98faa9cbe1a7e1a
ddaoellena <ddaoellena@gmail.com>
2015-11-10 22:06:42 +0100

    s3upload

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET']
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+      :aws_access_key_id      => "AKIAJENM62SN3JCPPW5Q",
+      :aws_secret_access_key  => "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"
+      :region                 => "ireland"
   }
-  config.fog_directory  = ENV['S3_BUCKET']
+  config.fog_directory  = "fondatelier"
 end
