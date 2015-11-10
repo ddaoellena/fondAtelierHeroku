5c8af69e3009241f4b970368ee702582b6405776
ddaoellena <ddaoellena@gmail.com>
2015-11-10 19:18:12 +0100

    namefixed

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
-
+ruby '2.2.3'
 gem 'rails', '4.1.8'
 gem 'pg'
 gem 'uglifier', '>= 1.3.0'
--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 #désactiver en local pour mailcatcher
-#ActionMailer::Base.smtp_settings = {
-# :address        => 'smtp.sendgrid.net',
-# :port           => '587',
-# :authentication => :plain,
-# :user_name      => ENV['SENDGRID_USERNAME'],
-# :password       => ENV['SENDGRID_PASSWORD'],
-# :domain         => 'www.fondatelier.fr',
-# :enable_starttls_auto => true
-#}
+ActionMailer::Base.smtp_settings = {
+ :address        => 'smtp.sendgrid.net',
+ :port           => '587',
+ :authentication => :plain,
+ :user_name      => ENV['SENDGRID_USERNAME'],
+ :password       => ENV['SENDGRID_PASSWORD'],
+ :domain         => 'www.fondatelier.fr',
+ :enable_starttls_auto => true
+}
