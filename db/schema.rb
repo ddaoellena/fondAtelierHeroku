bc2a75a9605e5c2bfb6fa26880084ee9f0ffcc76
ddaoellena <ddaoellena@gmail.com>
2015-11-12 14:40:05 +0100

    contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105182122) do
+ActiveRecord::Schema.define(version: 20151112132514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 20151105182122) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "ecoles", force: true do |t|
