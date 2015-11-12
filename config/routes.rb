1877af285e91c56962fc0c68087162f99e50fa94
ddaoellena <ddaoellena@gmail.com>
2015-11-12 15:31:36 +0100

    contact

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
 	get "/mes_annonces", to: "mes_annonces#index"
 	get "/static/cgu", to: "static#cgu"
 	get "/static/a_propos", to: "static#a_propos"
-	
-	match '/contacts', to: 'contacts#new', via: 'get'
 	resources "contacts", only: [:new, :create]
 
 end
