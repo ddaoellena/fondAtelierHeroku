993274695d25effcf9cf4b23b34ae7ddd2dd5f10
ddaoellena <ddaoellena@gmail.com>
2015-11-27 23:19:37 +0100

    omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ has_many :annonces
     	user.email = auth.info.email
     	user.password = Devise.friendly_token[0,20]
     	user.name = auth.info.name   # assuming the user model has a name
-    	user.image = auth.info.image # assuming the user model has an image
   		end
 	end
 
