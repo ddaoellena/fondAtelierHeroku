c5dd469a586874ff9dc79f91469766f10719502e
ddaoellena <ddaoellena@gmail.com>
2015-12-07 15:32:44 +0100

    css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,17 @@ class ApplicationController < ActionController::Base
       }
     end
   end
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
